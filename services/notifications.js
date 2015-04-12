093fec4bbcc452b31e536291dac5908d4d64ee77
Justin Manley <manleyjster@gmail.com>
2015-04-12 18:43:36 -0500

    Fix missing require() statements.

--- services/notifications.js
+++ services/notifications.js
@@ -1,17 +1,18 @@
+var cwd = process.cwd();
 require(cwd + "/lib/env");
-
-var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
-var message = [
-	'The current wind speed forecast at KCBX Terminals is ',
-	windSpeed.toString() + ' mph.', 
-	'??? see issue #2 ??? considers wind levels over ',
-	_.find(constants.WIND_STATUS_TERMS, 'term', 'Hazardous').threshold + ' mph ',
-	'to be hazardous. Read more at ' + constants.help_url + '.'
-];
+var	twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN),
+	_ = require('lodash');
 
 module.exports = {
 	send: function(windSpeed, callback, subscriberNumber) {
+		var message = [
+			'The current wind speed forecast at KCBX Terminals is ',
+			windSpeed.toString() + ' mph.', 
+			'??? see issue #2 ??? considers wind levels over ',
+			_.find(constants.WIND_STATUS_TERMS, 'term', 'Hazardous').threshold + ' mph ',
+			'to be hazardous. Read more at ' + constants.help_url + '.'
+		];
+
 		twilio.sendMessage({
 			to: subscriberNumber,
 			from: process.env.TWILIO_NUMBER,
