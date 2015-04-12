67214676907bf5e85dfebd96643dc56d29dc4f3d
Justin Manley <manleyjster@gmail.com>
2015-04-12 18:35:30 -0500

    Flesh out sms service for sending notifications about hazardous wind to subscribers using twilio.

--- services/notifications.js
+++ services/notifications.js
@@ -1,2 +1,21 @@
-var twilio = require('twilio');
+require(cwd + "/lib/env");
 
+var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+var message = [
+	'The current wind speed forecast at KCBX Terminals is ',
+	windSpeed.toString() + ' mph.', 
+	'??? see issue #2 ??? considers wind levels over ',
+	_.find(constants.WIND_STATUS_TERMS, 'term', 'Hazardous').threshold + ' mph ',
+	'to be hazardous. Read more at ' + constants.help_url + '.'
+];
+
+module.exports = {
+	send: function(windSpeed, callback, subscriberNumber) {
+		twilio.sendMessage({
+			to: subscriberNumber,
+			from: process.env.TWILIO_NUMBER,
+			body: message.join('')
+		}, callback);
+	}
+};
